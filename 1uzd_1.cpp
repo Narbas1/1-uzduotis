b50d7c00b5df3f7873b5de3f201c015d0962f17e
Narbas1 <karolis.narbutis@mif.stud.vu.lt>
2025-02-08 01:34:00 +0200

    pridetas naujas failas su masyvais vietoj vektoriu, pridetas checkinimas del dalybos is 0

--- 1uzd_1.cpp
+++ 1uzd_1.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <vector>
+#include <string>
 #include <ctime>
 #include <iomanip>
 #include <cstdlib>
@@ -7,14 +7,14 @@
 //struktura studentas
 
 struct studentas{
-    std::vector<float>pazymiai;
+    float* pazymiai;
+    int pKiekis;
     std::string vardas;
     std::string pavarde;
     float egzaminoRezultatas;
-    int pKiekis;
 };
 
-int main(){
+int main() {
 
     studentas studentas;
     std::cout << "Iveskite studento varda: " << std::endl;
@@ -22,13 +22,17 @@ int main(){
     std::cout << "Iveskite studento pavarde: " << std::endl;
     std::cin >> studentas.pavarde;
 
-    std::cout << "Iveskite pazymiu kieki" << std::endl;
+    std::cout << "Iveskite pazymiu kieki: " << std::endl;
     std::cin >> studentas.pKiekis;
 
     while(studentas.pKiekis <= 0){
         std::cout << "Pazymiu kiekis negali buti 0 arba neigiamas, iveskite teigiama skaiciu: " << std::endl;
         std::cin >> studentas.pKiekis;
     }
+
+    // dinaminis masyvas
+
+    studentas.pazymiai = new float[studentas.pKiekis];
 
     char pazymiuGenBudas;
     std::cout << "Ar norite atsitiktinai generuoti studento pazymius? t/n(taip/ne)" << std::endl;
@@ -44,21 +48,19 @@ int main(){
 
         for(int i = 0; i < studentas.pKiekis; i++){
             float randomPazymys = rand()%11;
-            studentas.pazymiai.push_back(randomPazymys);
+            studentas.pazymiai[i] = randomPazymys;
         }
-
+        
         studentas.egzaminoRezultatas = rand()%11;
 
     }
     else{
-    
+
         std::cout << "Iveskite pazymius: " << std::endl;
-    
+
         for(int i = 0; i < studentas.pKiekis; i++){
-            int pazymys = 0;
-            std::cout << "Pazymys " << i + 1 << ". ";
-            std::cin >> pazymys;
-            studentas.pazymiai.push_back(pazymys);
+            std::cout << "Pazymys " << i + 1 << ": ";
+            std::cin >> studentas.pazymiai[i];
         }
     
         std::cout << "Iveskite egzamino pazymi: " << std::endl;
@@ -66,6 +68,7 @@ int main(){
 
     }
 
+    
     float galutinisV;
     float galutinisM;
     float vidurkis;
@@ -83,8 +86,8 @@ int main(){
     else{
         vidurkis = suma/studentas.pKiekis;
     }
-    
-    //sorting
+
+    // sorting
 
     float temp;
 
@@ -98,42 +101,45 @@ int main(){
         }
     }
 
-    //mediana
+    // mediana
 
     float mediana = 0;
 
-    if(studentas.pKiekis % 2 == 0){
-        mediana = (studentas.pazymiai[studentas.pKiekis/2 - 1] + studentas.pazymiai[studentas.pKiekis/2])/2;
-    }
-    else{
-        mediana = studentas.pazymiai[(studentas.pKiekis+1)/2 - 1];
+    if(studentas.pKiekis % 2 == 0) {
+        mediana = (studentas.pazymiai[studentas.pKiekis / 2 - 1] + studentas.pazymiai[studentas.pKiekis / 2]) / 2.0;
+    } 
+    else {
+        mediana = studentas.pazymiai[(studentas.pKiekis + 1) / 2 - 1];
     }
 
-    //galutinis
+    // galutinis pazymys
 
-    galutinisV = vidurkis*0.4 + studentas.egzaminoRezultatas*0.6;
-    galutinisM = mediana*0.4 + studentas.egzaminoRezultatas*0.6;
+    galutinisV = vidurkis * 0.4 + studentas.egzaminoRezultatas * 0.6;
+    galutinisM = mediana * 0.4 + studentas.egzaminoRezultatas * 0.6;
 
     //lentele
 
     char pasirinkimas;
-    std::cout << "Ar norite skaiciuoti galutini ivertinima naudojant pazymiu vidurki ar mediana(v/m)?" << std::endl;
+    std::cout << "Ar norite skaiciuoti galutini ivertinima naudojant pazymiu vidurki ar mediana (v/m)? " << std::endl;
     std::cin >> pasirinkimas;
 
-    while(pasirinkimas != 'm' && pasirinkimas != 'v'){
-        std::cout << "Ivedete bloga simboli, iveskite is naujo: " << std::endl;
+    while(pasirinkimas != 'm' && pasirinkimas != 'v') {
+        std::cout << "Ivedete bloga simboli, bandykite dar karta (v/m): ";
         std::cin >> pasirinkimas;
     }
 
     if(pasirinkimas == 'v'){
-        std::cout << "Pavarde" << "   " << "Vardas" << "   " << "Galutinis(vid.)" << std::endl;
-        std::cout << "-------------------------------------" << std::endl;
-        std::cout << studentas.pavarde << "   " << studentas.vardas << "   " << galutinisV << std::endl;
+        std::cout << "Pavarde      Vardas       Galutinis(vid.)" << std::endl;
+        std::cout << "-----------------------------------------" << std::endl;
+        std::cout << studentas.pavarde << "      " << studentas.vardas << "            " << std::fixed << std::setprecision(2) << galutinisV << std::endl;
+    }   
+    else {
+        std::cout << "Pavarde      Vardas       Galutinis(med.)" << std::endl;
+        std::cout << "-----------------------------------------" << std::endl;
+        std::cout << studentas.pavarde << "      " << studentas.vardas << "            " << std::fixed << std::setprecision(2) << galutinisM << std::endl;
     }
-    else{
-        std::cout << "Pavarde" << "   " << "Vardas" << "   " << "Galutinis(med.)" << std::endl;
-        std::cout << "-------------------------------------" << std::endl;
-        std::cout << studentas.pavarde << "   " << studentas.vardas << "   " << galutinisM << std::endl;
-    }
+
+    delete[] studentas.pazymiai;
+
     return 0;
 }
